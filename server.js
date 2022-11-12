4c04b6786eb825ab9596e0b487b18fea914b0d17
Mudit <muditwadhwa1@gmail.com>
2022-11-13 00:12:34 +0530

    Check Deployment

--- server.js
+++ server.js
@@ -21,27 +21,27 @@ app.use('/api/pizzas',require('./routes/pizzaRoute'));
 app.use('/api/users',require('./routes/userRoutes'));
 app.use('/api/orders',require('./routes/orderRoute'));
 
-// if(process.env.NODE_ENV==='production')
-// {
-//   app.use(express.static(path.join(__dirname,'/client/build')))
-//    app.get('*',(req,res)=>{
-//        res.sendFile(path.resolve(__dirname,"client","build","index.html"),)
-//    })
-// }else
-// {
-//     app.get('/',(req,res)=>{
-//         res.send("<h1>Hello from Node Server</h1>")
-//     })
-// }
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+if(process.env.NODE_ENV==='development')
+{
+  app.use(express.static(path.join(__dirname,'/client/build')))
+   app.get('*',(req,res)=>{
+       res.sendFile(path.resolve(__dirname,"client","build","index.html"),)
+   })
+}else
+{
+    app.get('/',(req,res)=>{
+        res.send("<h1>Hello from Node Server</h1>")
+    })
+}
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       res.status(500).send(err);
+//     }
+//   );
+// });
 
 const port=process.env.PORT||8080;
 
